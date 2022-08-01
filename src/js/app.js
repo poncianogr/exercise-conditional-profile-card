9b55728298b53cb04a7a7584d77051eee2b59df4
poncianogr <poncianogr@gmail.com>
2022-08-01 22:32:31 +0000

    cambios

--- src/js/app.js
+++ src/js/app.js
@@ -14,7 +14,6 @@ import "../style/index.css";
         github: null,
         linkedin: null,
         instagram: null,
-
         name: null,
         lastname: null,
         role: null,
@@ -28,19 +27,48 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  // Name and Last name
+  if (variables.name == null) variables.name = "Name";
+  if (variables.lastname == null) variables.lastname = "LastName";
+
+  //Social Media links
+  //Twitter
+  if (variables.twitter != null) variables.twitter;
+  //Github
+  if (variables.github != null) variables.github;
+  //Linkedin
+  if (variables.linkedin != null) variables.linkedin;
+  //IG
+  if (variables.instagram != null) variables.instagram;
+  //Social Media Position
+
+  // Role
+  if (variables.role == null) variables.role = "Role";
+
+  // Country & City
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name} ${variables.lastname}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${
+            variables.city == null ? "No hay ciudad" : variables.city
+          } ${variables.country || "No hay país"}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${
+              variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/in/${
+              variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
